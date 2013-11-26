fa43d381281f0a1c17f5e748f3d4d253486e3d45
Pedro Pereira <pedro_aka_peter@live.com.pt>
2013-11-26 18:01:26 +0000

    Upload do projecto para github (first and last time)

---
+++ IDEIBiblio/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IDEIBiblio.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
